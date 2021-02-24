4998aeee54f7ffc58498191d81e9412c071b9664
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-25 03:16:21 +0800

    add routes to backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import products from "./routes/product.js";
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/v1", products);
 
 export default app;
