e651690ec4ca1f767a12013a34db94ed0826b776
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-25 02:09:08 +0800

    setup packages & express app

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+/* const app = require("./app");
+const dotenv = require("dotenv"); */
+import app from "./app.js";
+import dotenv from "dotenv";
+
+// setting up config file
+dotenv.config({ path: "backend/config/config.env" });
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `App is listening to port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
