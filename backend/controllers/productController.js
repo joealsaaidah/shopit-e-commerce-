51cac9413f8f8cbb89c79496256329484754806b
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-25 17:27:43 +0800

    Add Get all product & add new product routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,20 @@
-export const getProducts = (req, res, next) => {
+import Product from "../models/product.js";
+
+// Create a new Product => /api/v1/produc/new
+export const newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// fetch all products live in the database => api/v1/products
+export const getProducts = async (req, res, next) => {
+  const products = await Product.find();
   res.status(200).json({
-    success: "true",
-    messgage: "This route will fetch all products from the Database",
+    success: true,
+    count: products.length,
+    products,
   });
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProducts } from "../controllers/productController.js";
+import { getProducts, newProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
+router.post("/product/new", newProduct);
 
 export default router;
