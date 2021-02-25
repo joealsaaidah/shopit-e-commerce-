5c50ea70b5ca093bad16799874c36d474aa7726d
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-26 00:34:53 +0800

    error: mongoose cast error & validation error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -16,6 +16,18 @@ export default (err, req, res, next) => {
 
     error.message = err.message;
 
+    //wrong mangoose object ID Error
+    if (err.name === "CastError") {
+      const message = `Resources not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handlling mangoose validation ErrorHandler
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
