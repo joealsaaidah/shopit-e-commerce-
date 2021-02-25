2354d3904f1be92c7a2410a4f1739587de6319a3
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-26 00:06:59 +0800

    error: unhandle rejection & uncaught exception

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,30 @@ import app from "./app.js";
 import dotenv from "dotenv";
 import connecttoDatabase from "./config/database.js";
 
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught Exception`);
+  process.exit(1);
+});
+
 // setting up config file
 dotenv.config({ path: "backend/config/config.env" });
 
 // connect to Database
 connecttoDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `App is listening to port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+// Handle unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to unhandle promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
