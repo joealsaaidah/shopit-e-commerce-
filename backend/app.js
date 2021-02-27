e32d126eec3c44f6283b016c4a885fc1bc12296d
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-27 22:33:45 +0800

    Order: create model, and all routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import products from "./routes/product.js";
 import errorMeddleware from "./middlewares/errors.js";
 import users from "./routes/user.js";
 import cookieParser from "cookie-parser";
+import orders from "./routes/order.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", products);
 app.use("/api/v1", users);
+app.use("/api/v1", orders);
 
 //middleware to handle Errors
 app.use(errorMeddleware);
