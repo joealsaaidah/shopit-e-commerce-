4248163ebff8852453bf3a1fb603cd0e3fa79f4c
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-28 00:42:23 +0800

    Reviews: create review, update and delete

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,6 +5,9 @@ import {
   getSingleProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } from "../controllers/productController.js";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
@@ -21,4 +24,8 @@ router
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
+router.route("/review").put(isAuthenticatedUser, createProductReview);
 export default router;
+
+router.route("/reviews").get(isAuthenticatedUser, getProductReviews);
+router.route("/reviews").delete(isAuthenticatedUser, deleteReview);
