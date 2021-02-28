4aa9a2d20aa16e838be88996317d8f57d8ea6376
joealsaaidah <alsaaidahyousef@gmail.com>
2021-02-28 16:50:44 +0800

    setup react project and also add helmet package

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Home from "./components/Home";
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Header />
+        <div className='container container-fluid'>
+          <Route path='/' component={Home} exact />
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
